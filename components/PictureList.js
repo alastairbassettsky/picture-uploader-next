4a15a48e1162eb5976fb58d3bb87bc02aaba2aae
alastair.bassett <alastair.bassett@sky.uk>
2020-04-03 14:42:29 +0100

    Breaking things

--- components/PictureList.js
+++ components/PictureList.js
@@ -21,7 +21,7 @@ export const PictureList = () => {
 
     const [pictures, setPictures] = useState([]);
 
-    // getAllPictureNames();
+    getAllPictureNames();
 
     return (
         <div className="picture-list">
